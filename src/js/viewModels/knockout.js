0d9e137451d6645b760a137402835666558117c6
Alex Chen <xiaojuch@dhcp-10-8-105-165.cn.oracle.com>
2019-09-16 15:05:50 +0800

    knockout

--- src/js/viewModels/knockout.js
+++ src/js/viewModels/knockout.js
@@ -3,6 +3,9 @@ define(['knockout'],
   function (ko) {
     console.log("KnockoutViewModel ==> 1");
     function KnockoutViewModel(first, last) {
+
+      var self = this;
+
       this.firstName = ko.observable(first);
       this.lastName = ko.observable(last);
       this.fullName = ko.pureComputed(function () {
@@ -108,8 +111,66 @@ define(['knockout'],
 
       this.myMessage = "Hello, world";
       this.price = ko.observable(52.95);
-      this.priceRating = ko.pureComputed(function(){
-          return this.price > 50 ? "expensive" : "affordable";
+      this.priceRating = ko.pureComputed(function () {
+        return this.price > 50 ? "expensive" : "affordable";
+      });
+
+      //////////////////HTML Binding//////////////////////
+      this.htmlDetails = ko.observable("");
+      // HTML content appears
+      this.htmlDetails = "<em>For further details, view the report <a href='html/testHtml.html'>here</a>.</em>";
+
+      //////////////////attr binding//////////////////////
+      this.attrUrl = ko.observable("");
+      this.attrUrl = "html/testHtml.html";
+
+      this.attrDetails = ko.observable("");
+      this.attrDetails = "Report including final year-end statistics";
+
+      //////////////////for each binding//////////////////////
+      self.people = ko.observableArray(
+        [{ firstName: 'Bert', lastName: 'Bertington' },
+        { firstName: 'Charles', lastName: 'Charlesforth' },
+        { firstName: 'Denise', lastName: 'Dentiste' }
+        ]
+      );
+      self.addPerson = function () {
+        self.people.push({ firstName: 'Alex', lastName: 'Chen' });
+      };
+      self.removePerson = function (event, current, bindingContext) {
+        self.people.remove(this);
+
+        console.log("current: " + this.firstName + "  " + this.lastName);
+        //this.people.remove(this);
+      }
+
+      //////////////////for each binding//////////////////////
+      this.displayMessage = ko.observable("false");
+
+      //////////////////Using and With//////////////////////
+      self.twitterName = ko.observable('@example');
+      self.resultData = ko.observable(); // No initial value
+
+      self.getTweets = function () {
+        var name = self.twitterName(),
+          simulatedResults = [
+            { text: name + ' What a nice day.' },
+            { text: name + ' Building some cool apps.' },
+            { text: name + ' Just saw a famous celebrity eating lard. Yum.' }
+          ];
+        self.resultData({ retrievalDate: new Date(), topTweets: simulatedResults });
+      }
+      self.clearResults = function () {
+        self.resultData(undefined);
+      }
+
+      //////////////////Component Binding//////////////////////
+      ko.components.register('message-editor', {
+        viewModel: function (params) {
+          this.text = ko.observable(params && params.initialText || '');
+        },
+        template: 'Message: <input data-bind="value: text" /> '
+          + '(length: <span data-bind="text: text().length"></span>)'
       });
 
     }
